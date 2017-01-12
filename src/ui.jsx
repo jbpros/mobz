df43b55c157ce19f1619db26a09e2eada735581a
Julien Biezemans <jb@jbpros.com>
2017-01-12 23:22:35 +0100

    Introduce redux

--- src/ui.jsx
+++ src/ui.jsx
@@ -1,9 +1,23 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+const { createStore } = require('redux')
+
+function reducer(state, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+class Roomz extends React.Component {
+  render() {
+    return <h1>roomz</h1>
+  }
+}
 
 ReactDOM.render(
-  <h1>roomz</h1>,
+  <Roomz />,
   document.getElementById('app')
 )
-
-console.log('cocuou')
