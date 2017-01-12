a197f569f6b55db5e6b88bec1d243f5ad28ca3f5
Julien Biezemans <jb@jbpros.com>
2017-01-12 22:41:10 +0100

    Extract UI JS to own file

---
+++ ui.js
@@ -0,0 +1,9 @@
+// const React = require('react')
+// const ReactDOM = require('react-dom')
+
+const e = React.createElement
+
+ReactDOM.render(
+  e('h1', null, 'roomz'),
+  document.getElementById('app')
+)
