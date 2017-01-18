5df17e6a2403f1c015abd0d1d31f1c4ba3b4b72b
Julien Biezemans <jb@jbpros.com>
2017-01-18 09:07:10 +0100

    Extract UserDetailsForm

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,38 +1,8 @@
 const React = require('react')
 const { connect } = require('react-redux')
+const UserDetailsForm = require('./UserDetailsForm')
 const { pickUserDetails } = require('../pickers')
 const { setUserDetails } = require('../actions')
-
-class UserDetailsForm extends React.Component {
-  constructor() {
-    super()
-    this.state = { email: '' }
-  }
-
-  handleChange(e) {
-    this.setState({ email: e.target.value })
-  }
-
-  handleSubmit(e) {
-    e.preventDefault()
-    this.props.onSubmit({ email: this.state.email })
-  }
-
-  render() {
-    return <form onSubmit={::this.handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        value={this.state.email}
-        onChange={::this.handleChange}></input>
-    </form>
-  }
-}
-
-UserDetailsForm.propTypes = {
-  onSubmit: React.PropTypes.func.isRequired
-}
 
 // TODO: remember details between app runs
 class UserDetails extends React.Component {
---
+++ src/components/UserDetailsForm.jsx
@@ -0,0 +1,34 @@
+const React = require('react')
+
+class UserDetailsForm extends React.Component {
+  constructor() {
+    super()
+    this.state = { email: '' }
+  }
+
+  handleChange(e) {
+    this.setState({ email: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.onSubmit({ email: this.state.email })
+  }
+
+  render() {
+    return <form onSubmit={::this.handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={this.state.email}
+        onChange={::this.handleChange}></input>
+    </form>
+  }
+}
+
+UserDetailsForm.propTypes = {
+  onSubmit: React.PropTypes.func.isRequired
+}
+
+module.exports = UserDetailsForm
