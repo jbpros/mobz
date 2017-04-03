a51dea5acb36699cd10e52c0ed299e84b2c568ec
Julien Biezemans <jb@jbpros.com>
2017-04-03 21:58:33 +0200

    Refactor

--- src/elm/View/App.elm
+++ src/elm/View/App.elm
@@ -3,24 +3,24 @@ module View.App exposing (view)
 import Html exposing (..)
 import Html.Events exposing (onInput, onSubmit, onClick)
 import Model.Main exposing (..)
-import Model.Status exposing (..)
-import Msg.Main as Main exposing (..)
-import Msg.Status exposing (..)
+import Model.Status as Status
+import Msg.Main exposing (..)
+import Msg.Status
 import View.Gravatar as Gravatar exposing (view)
 import View.StatusForm as StatusForm exposing (view)
 import View.StatusMessage as StatusMessage exposing (view)
 
 
-view : Model.Main.Model -> Html Main.Msg
+view : Model -> Html Msg
 view model =
     let
         ( status, toggleStatus, toggleLabel ) =
             case model.status of
-                Active ->
-                    ( "active", SetInactive, "Stop paying attention" )
+                Status.Active ->
+                    ( "active", Msg.Status.SetInactive, "Stop paying attention" )
 
-                Inactive ->
-                    ( "inactive", SetActive, "Pay attention" )
+                Status.Inactive ->
+                    ( "inactive", Msg.Status.SetActive, "Pay attention" )
     in
         if model.userSettings.ready then
             div []
@@ -35,7 +35,7 @@ view model =
                     , text status
                     , StatusMessage.view model.userSettings.statusMessage
                     ]
-                , button [ onClick (Main.MsgForStatus toggleStatus) ]
+                , button [ onClick (MsgForStatus toggleStatus) ]
                     [ text toggleLabel ]
                 , StatusForm.view model.userSettings
                 ]
